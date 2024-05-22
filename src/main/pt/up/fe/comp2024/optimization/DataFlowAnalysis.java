f63cf5b811d303004e007d333614f5a3cbcb1b4f
Tomas Camara <tomas.camara03@gmail.com>
2024-05-22 23:19:54 +0100

    Did computeDef in DataFlowAnalysis, commented method computeUse which still has to be done

---
+++ src/main/pt/up/fe/comp2024/optimization/DataFlowAnalysis.java
@@ -0,0 +1,70 @@
+package pt.up.fe.comp2024.optimization;
+
+import org.specs.comp.ollir.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataFlowAnalysis {
+
+    private Method method;
+    private Map<Node, List<Operand>> def;
+    private Map<Node, List<Operand>> use;
+    private Map<Node, List<Operand>> in;
+    private Map<Node, List<Operand>> out;
+
+    public DataFlowAnalysis(Method method) {
+        this.method = method;
+        this.def = new HashMap<>();
+        this.use = new HashMap<>();
+        this.in = new HashMap<>();
+        this.out = new HashMap<>();
+    }
+
+    public void run() {
+
+        for (Instruction instruction : method.getInstructions()) {
+            def.put(instruction, computeDef(instruction, method.getVarTable()));
+            //use.put(instruction, computeUse(instruction, method.getVarTable()));
+
+        }
+    }
+
+    private List<Operand> computeDef(Instruction instruction, Map<String, Descriptor> varTable) {
+        List<Operand> def = new ArrayList<>();
+
+        if (instruction.getInstType() == instruction.getInstType().ASSIGN) {
+            AssignInstruction assignInstruction = (AssignInstruction) instruction;
+            Element dest = assignInstruction.getDest();
+            //Type type = dest.getType();
+
+            if (dest.isLiteral()) {
+                return null;
+            }
+
+            def.add((Operand) dest);
+        }
+
+        return def;
+    }
+
+    /*private List<Operand> computeUse(Instruction instruction) {
+        List<Operand> use = new ArrayList<>();
+
+        /*switch (instruction.getInstType()) {
+            case ASSIGN ->
+            case CALL ->
+            case GOTO ->
+            case BRANCH ->
+            case RETURN ->
+            case PUTFIELD ->
+            case GETFIELD ->
+            case UNARYOPER ->
+            case BINARYOPER ->
+            case NOPER ->
+        }
+    }*/
+}
+
--- src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
+++ src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
@@ -1,10 +1,14 @@
 package pt.up.fe.comp2024.optimization;
 
+import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.Descriptor;
+import org.specs.comp.ollir.Method;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 
 import java.util.Collections;
+import java.util.HashMap;
 
 public class JmmOptimizationImpl implements JmmOptimization {
 
@@ -19,9 +23,27 @@ public class JmmOptimizationImpl implements JmmOptimization {
 
     @Override
     public OllirResult optimize(OllirResult ollirResult) {
+        int numReg = ollirResult.getConfig().containsKey("registerAllocation") ? Integer.parseInt(ollirResult.getConfig().get("registerAllocation")) : -1;
         boolean optimize1 = ollirResult.getConfig().containsKey("optimize");
-        if(optimize1){
-            if(ollirResult.getConfig().get("optimize").equals("true")){
+
+        if (numReg >= 0) { // Register Allocation
+            for (Method method : ollirResult.getOllirClass().getMethods()) {
+                method.buildCFG();
+                method.buildVarTable();
+
+                DataFlowAnalysis dataFlowAnalysis = new DataFlowAnalysis(method);
+                dataFlowAnalysis.run();
+
+                HashMap<String, Descriptor> old_varTable = method.getVarTable();
+
+
+
+                System.out.println("Hello world");
+            }
+        }
+
+        if (optimize1) {
+            if (ollirResult.getConfig().get("optimize").equals("true")) {
                 //TODO OPTIMIZE HERE
             }
         }
